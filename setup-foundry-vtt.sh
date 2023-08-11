cae2318e2be6a42b3d823c1d045cfb663d4cd7e4
Patrick Karjala <pkarjala@gmail.com>
2023-08-11 09:55:18 -1000

    Update default NodeJS version based on 18.x LTS.

--- setup-foundry-vtt.sh
+++ setup-foundry-vtt.sh
@@ -82,8 +82,8 @@ echo -e "Preparing to install Node.js."
 if command -v node >/dev/null; then
 	echo -e "${cyan}[INFO]${coloroff} Node.js is already installed; skipping."
 else
-	echo -e "${green}[OK]${coloroff} Installing the current LTS version of Node.js"
-	curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+	echo -e "${green}[OK]${coloroff} Installing the current LTS version (18.x) of Node.js"
+	curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 	sudo apt-get install -y nodejs
 fi
 
